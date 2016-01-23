f0cc5f487e12bcd1c62045262e6116e5810ff62b
vagrant <vagrant@precise32.(none)>
2016-01-23 19:13:49 +0000

    BlogPost-2: Multithreading Image Duplication Detection

---
+++ blogpost-2-multithreading/file_finder.py
@@ -0,0 +1,31 @@
+#BlogPost: http://enamulhaq.com/blog/avoid-picture-duplication---reclaim-your-space
+
+import os
+
+class FileFinder(object):
+ 
+    @classmethod
+    def ifind(self, src_path, filter_exts=None, 
+             ext_delimiter='.'):
+        '''
+            Function given the directory path and extension, it'll
+            reutrn the generator to iterator the list of files found
+        '''
+        if filter_exts and not isinstance(filter_exts, list):
+            raise TypeError('filter_exts should be a list of extensions')
+ 
+        for dirpath, subdirs, filenames in os.walk(src_path):
+            for fn in filenames:
+                if filter_exts:
+                    if fn[ fn.rfind(ext_delimiter) + 1:] in filter_exts:
+                        yield os.path.join(dirpath, fn)
+                else:
+                    yield os.path.join(dirpath, fn)
+
+class ImageFinder(object):
+    
+    filters_ext = ['jpg', 'jpeg', 'JPG']
+
+    @classmethod
+    def ifind(cls, src_path):
+        return FileFinder.ifind(src_path, cls.filters_ext)
---
+++ blogpost-2-multithreading/image_hasher.py
@@ -0,0 +1,139 @@
+import os
+import sys
+import time
+from collections import defaultdict
+from Queue import Queue, Empty
+from threading import Thread, Event, Condition, current_thread
+
+from file_finder import ImageFinder
+from photohash.photohash import average_hash
+
+class ImageHasher(object):
+
+    def __init__(self, src_path, workers_count=5):
+        
+        #image directory path
+        self.src_path = src_path
+
+        #inbox: queue to store unprocessed image 
+        #from which threads will read the unprocessed image path
+        self.inbox = Queue()
+
+        #queue where threads will write the calculated hash and
+        #the path of image as a tuple
+        self.outbox = Queue()
+
+        #failed images will be logged here
+        self.error = Queue()
+
+        #event to watch for shutdown
+        self.shutdown = Event()
+
+        #event to indicate workers that 
+        #we are done sending data
+        self.done = Event()
+
+        #condition to signal when queue is not empty
+        self.empty = Condition()
+
+        self.workers = []
+        self.workers_count = workers_count
+
+    def _worker(self):
+        '''
+        This is the worker which will get the image from 'inbox',
+        calculate the hash and puts the result in 'outbox'
+        '''
+
+        while not self.shutdown.isSet():
+            
+            try:
+                image_path = self.inbox.get_nowait()
+            except Empty:
+                print 'no data found. isset: ' , self.done.isSet()
+                if not self.done.isSet():
+                    with self.empty:
+                        self.empty.wait()
+                        continue
+                else:
+                    break
+
+            if not os.path.exists(image_path):
+                self.error((image_path, 'Image Does not Exist'))
+                
+            try:
+                print '[%s] Processing %s' % (current_thread().ident, image_path)
+                image_hash = average_hash(image_path)
+                self.outbox.put((image_hash, image_path))
+            except IOError as err:
+                print 'ERROR: Got %s for image : %s' % (image_path, err)
+        print 'Worker %s has done processing.' % current_thread().ident
+
+    def _start_workers(self):
+        '''method to start all the worker threads'''
+        for _ in range(self.workers_count):
+            worker = Thread(target=self._worker)
+            worker.start()
+            self.workers.append(worker)
+
+    def start(self):
+        
+        #lets start workers
+        self._start_workers()
+        print 'Workers are started. Waiting for work...'
+
+        #find images to put in inbox, workers are waiting
+        images_path = ImageFinder.ifind(self.src_path)
+        for image_path in images_path:
+            print 'Found image: %s' % image_path
+            with self.empty:
+                #acquire the condition lock, put image path in inbox queue
+                #and notify the worker thread who is waiting for an item to be put in the inbox queue
+                self.inbox.put_nowait(image_path)
+                self.empty.notify()
+
+        #lets tell every worker we are done sending data
+        #and exit once they are get Empty exception
+        self.done.set()
+
+        print 'All images has been sent to worker. Waiting to finish'
+        #all we have to do now is wait for
+        #workers to finish processing
+        for worker in self.workers:
+            worker.join()
+            print 'Worker %s has done processing' % (worker.ident)
+
+        #now that we have all the images processed
+        #lets see how many duplicates are there
+        dupe_images = defaultdict(list)
+        while True:
+            try:
+                (image_hash, image_path) = self.outbox.get_nowait()
+                dupe_images[image_hash].append(image_path)
+            except Empty:
+                break
+
+        for _hash, _paths in dupe_images.iteritems():
+            print 'Hash: %s DupeCount: %s Paths: %s' % (_hash, len(_paths), ','.join(_paths))
+
+def main():
+
+    src_path = '/vagrant/my_pictures/Family Pictures/'
+    workers_count = 5
+    if len(sys.argv) >= 2:
+        workers_count = int(sys.argv[1])
+    print 'Workers Count: %s' % (workers_count)
+
+    hasher = ImageHasher(src_path, workers_count)
+    try:
+        hasher.start()
+    except KeyboardInterrupt:
+        print 'Got Shutdown. Stopping'
+        hasher.shutdown.set()
+        with hasher.empty:
+            hasher.empty.notifyAll()
+
+if __name__ == '__main__':
+    
+    sys.exit(main())
+    
